fce4dcf481f2b22480fa9945b299a034dc9aa513
7i7o <mparij@gmail.com>
2021-12-27 17:11:32 -0300

    ERC-1155 Deployment info + assets

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -14,7 +14,7 @@ const app = sdk.getAppModule("0x0DAF0fcda387A4026283152853B289a5975A6A91");
       // The collection's name, ex. CryptoPunks
       name: "MateDAO Membership",
       // A description for the collection.
-      description: "A DAO for mate drinkers.",
+      description: "A DAO for mate drinkers (ú,🚀)",
       // The image for the collection that will show up on OpenSea.
       image: readFileSync("scripts/assets/mate.png"),
       // We need to pass in the address of the person who will be receiving the proceeds from sales of nfts in the module.
